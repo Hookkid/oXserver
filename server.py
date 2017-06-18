a62f1db13acecd3222e4662a2cc5de7c4a123078
Bruno Portela <bportela@outlook.com>
2017-06-18 16:38:24 -0300

    fixing up environment variables, adjusting naming, adding collection listing

--- server.py
+++ server.py
@@ -6,50 +6,68 @@ from flask_cors import CORS, cross_origin
 from bson import Binary, Code
 from bson.json_util import dumps
 from bson.json_util import loads
+import env #Create your own env.py with your mongodb credencials
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['MONGO_DBNAME'] = 'Cluster0'
-app.config['MONGO_URI'] = 'mongodb://bportela:sucess00@cluster0-shard-00-00-8gxhe.mongodb.net:27017,cluster0-shard-00-01-8gxhe.mongodb.net:27017,cluster0-shard-00-02-8gxhe.mongodb.net:27017/Cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
-
+app.config['MONGO_DBNAME'] = env.mongoDbName
+app.config['MONGO_URI'] =  env.MongoUri
 mongo = PyMongo(app)
 
 @app.route('/', methods=['GET'])
-def get_all_stars():
-  star = mongo.db.stars
+def get_all_records():
+  result = mongo.db.stars
   output = []
-  for s in star.find():
+  for s in result.find():
     output.append({'key': dumps(s['_id']), 'name' : s['name'], 'distance' : s['distance']})
   return jsonify({'result' : output})
 
-@app.route('/<name>', methods=['GET'])
-def get_one_star(name):
-  star = mongo.db.stars
-  s = star.find_one({'name' : name})
+@app.route('/key/<name>', methods=['GET'])
+def get_one_record(name):
+  result = mongo.db.stars
+  s = result.find_one(loads(name))
   if s:
-    output = {'name' : s['_id'], 'distance' : s['distance']}
+    output = {'key': name, 'name' : s['name'], 'distance' : s['distance']}
   else:
     output = "No such name"
   return jsonify({'result' : output})
 
 @app.route('/', methods=['POST'])
-def add_star():
-  star = mongo.db.stars  
+def add_record():
+  result = mongo.db.stars  
   name = request.json['name']
   distance = request.json['distance']
-  star_id = star.insert({'name': name, 'distance': distance})
-  new_star = star.find_one({'_id': star_id })
+  star_id = result.insert({'name': name, 'distance': distance})
+  new_star = result.find_one({'_id': star_id })
   output = {'key': dumps(star_id), 'name' : new_star['name'], 'distance' : new_star['distance']}
   return jsonify({'result' : output})
 
-@app.route('/delete', methods=['POST'])
-def del_star():
-  star = mongo.db.stars  
-  key = request.json
-  star.remove(loads(key))
+@app.route('/update/', methods=['POST'])
+def update_record():
+  result = mongo.db.stars  
+  key = request.json['key']
+  name = request.json['name']
+  distance = request.json['distance']
+  result.replace_one( {'_id': loads(key)} , {'name': name, 'distance': distance})
   output = {'key': key}
   return jsonify({'result' : output})
+
+@app.route('/delete', methods=['POST'])
+def del_record():
+  result = mongo.db.stars  
+  key = request.json
+  result.remove(loads(key))
+  output = {'key': key}
+  return jsonify({'result' : output})
+
+  ## RETRIEVEING LIST OF DBS
+@app.route('/dbs', methods=['GET'])
+def get_all_dbs():
+  databases = mongo.db.collection_names()
+  return jsonify({'result' : databases})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
